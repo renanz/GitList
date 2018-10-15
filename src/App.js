a39f2f68ee8dffba4533d045b48da62ab04e9f89
renanz <renanz@RENANZELAYA-PC.localdomain>
2018-10-15 16:25:20 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Lista de proyectos de Github")
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
